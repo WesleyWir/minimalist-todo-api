84fdc806437166e485ea8520648a3576ea47e15e
Wesley Wirmond <wesleywirmond@gmail.com>
2022-11-27 00:46:50 -0300

    prisma setup and some general adjusts

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,9 @@
 export default {
-    port: process.env.PORT || 3001
+    port: process.env.PORT || 3001,
+    db_connection: process.env.DB_CONNECTION || 'mysql',
+    db_host: process.env.DB_HOST || 'localhost',
+    db_port: process.env.DB_PORT || 3306,
+    db_database: process.env.DB_DATABASE,
+    db_username: process.env.DB_USERNAME || 'root',
+    db_password: process.env.DB_PASSWORD || 'secret'
 }
